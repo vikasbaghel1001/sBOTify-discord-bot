20c8f97ee09bfa3cdb5732b8df41b08d902b06a3
vikas baghel <75917410+vikasbaghel1001@users.noreply.github.com>
2021-10-28 14:52:00 +0530

    Create data used .py

---
+++ functions/data used .py
@@ -0,0 +1,124 @@
+thanks_words = ["thanks",
+"gratitude",
+"acknowledgment",
+"benediction",
+"blessing",
+"credit",
+"grace",
+"gratefulness",
+"praise",
+"thankfulness",
+"thanksgiving",
+"gramercy" ]
+
+normal_words =["really","actually",
+"authentically",
+"certifiably",
+"genuinely",
+"truly",
+"veritably",
+"very"]
+
+sad_words =  ["bad","guilty", " sad", "lonely","unhappy", "depressed", 
+ "help",
+ "bitter",
+ "dismal",
+ "heartbroken",
+ "melancholy",
+ "mournful",
+ "pessimistic",
+ "somber",
+ "sorrowful",
+ "sorry",
+ "wistful",
+ "bereaved",
+ "blue",
+ "cheerless",
+ "dejected",
+ "despairing",
+  "despondent",
+ "disconsolate",
+ "distressed",
+ "doleful",
+ "down",
+ "down in dumps",
+ "down in mouth",
+"downcast",
+ "forlorn",
+ "gloomy",
+ "glum",
+ "grief-stricken",
+ "grieved",
+ "heartsick",
+ "heavyhearted",
+ "hurting",
+ "in doldrums",
+ "in grief",
+ "in the dumps",
+ "languishing",
+ "low",
+ "low-spirited",
+ "lugubrious",
+ "morbid",
+ "morose"]
+
+start_engoragements = ["Give it a try.",
+ "Go for it.",
+ "Why not?",
+ "It’s worth a shot.",
+ "What are you waiting for?",
+ "What do you have to lose?",
+ "You might as well.",
+ "Just do it.",
+ "There you go!",
+ "Keep up the good work.",
+ "Keep it up.",
+ "Good job.",
+ "I’m so proud of you!",
+ "Hang in there.",
+ "Don’t give up.",
+ "Keep pushing.",
+ "Keep fighting!",
+ "Stay strong.",
+ "Never give up.",
+ "Never say ‘die’.",
+ "Come on! You can do it",
+ "I’ll support you either way.",
+ "I’m behind you 100%.",
+ "It’s totally up to you..",
+ "It’s your call.",
+ "Follow your dreams.",
+ "Reach for the stars.",
+ "Do the impossible.",
+ "Believe in yourself.",
+ "The sky is the limit"]
+
+random_jokes = [
+   "What do Alexander the Great and Winnie the Pooh have in common? Same middle name.",
+   "My dog used to chase people on a bike a lot. It got so bad, finally I had to take his bike away",
+   "I wasn't that drunk yesterday." "Oh boy you took the shower head in your arms and told it to stop crying.",
+   "I saw a poster today, somebody was asking “Have you seen my cat?” So I called the number and said that I didn’t. - I like to help where I can",
+   "My dog is an awesome fashion adviser. Every time I ask him what I look like in my clothes, he says “WOW!"
+
+]
+
+pj = [
+  "Why do Java developers often wear glasses? They can’t C#",
+
+"A SQL query walks into a bar, goes up to two tables and says “can I join you " ,
+
+
+"I love pressing the F5 key. It’s refreshing.",
+
+"There are 10 types of people in the world. Those who understand binary and those who don’t.",
+
+"Why was the developer bankrupt? He’d used all his cache.",
+
+"A friend is in a band called 1023Mb. They haven’t had a gig yet.",
+
+"If you listen to a UNIX shell, can you hear the C?",
+
+"Why do programmers confuse Halloween with Christmas? Because 31OCT=25DEC.",
+
+"What is the difference between a programmer and a non-programmer? The non-programmer thinks a kilobyte is 1000 bytes while a programmer is convinced that a kilometre is 1024 metres."
+]
